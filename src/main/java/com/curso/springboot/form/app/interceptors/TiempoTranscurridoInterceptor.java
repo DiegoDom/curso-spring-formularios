d0b9c6a3f698cf5def623b944f468784c43c6eda
DiegoDom <dominguezdiex@gmail.com>
2022-04-01 13:05:40 -0600

    Http interceptors ejemplo

---
+++ src/main/java/com/curso/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,67 @@
+package com.curso.springboot.form.app.interceptors;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component("tiempoTranscurridoInterceptor")
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+
+	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		if (request.getMethod().equalsIgnoreCase("post")) {
+			return true;
+		}
+
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod metodo = (HandlerMethod) handler;
+			logger.info("Metodo del controlador: " + metodo.getMethod().getName());
+		}
+
+		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando...");
+
+		long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio", tiempoInicio);
+
+		// Se asigna un tiempo extra de demora
+		Random random = new Random();
+		Integer delay = random.nextInt(500);
+		Thread.sleep(delay);
+
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		if (request.getMethod().equalsIgnoreCase("post")) {
+			return;
+		}
+
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+
+		long tiempoTranscurrido = tiempoFin - tiempoInicio;
+
+		if (handler instanceof HandlerMethod && modelAndView != null) {
+			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+		}
+
+		logger.info("Tiempo transcurrido: " + tiempoTranscurrido + " milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo...");
+	}
+
+}
